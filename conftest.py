98a84b9c488ccf150b2320b9433b1288d9f71bd4
Usama Sadiq <usama7274@gmail.com>
2026-01-05 17:32:05 +0500

    chore: upgrade python requirements

--- conftest.py
+++ conftest.py
@@ -3,10 +3,10 @@ File configures pytest for this repo
 """
 
 
-def pytest_ignore_collect(path, config):
+def pytest_ignore_collect(collection_path, config):
     """
     Pytest hook to determine if tests should be collected in `path`.
     """
-    if "{" in str(path) and "}" in str(path):
+    if "{" in str(collection_path) and "}" in str(collection_path):
         return True
     return None
